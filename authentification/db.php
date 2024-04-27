b5c9b024b3e52c4ca7d62697f1aca349e25abbd4
Ronan Scotet <ronanscotet467@gmail.com>
2024-04-27 12:18:52 +0200

    [ADD] GITHUB ACTIONS CI DEPLOY

--- authentification/db.php
+++ authentification/db.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASSWORD", "");
-define("DB_NAME", "zechifoumi");
+define("DB_USER", "bfxr3414_adminsupreme");
+define("DB_PASSWORD", "AIo(Nw,*L=k,");
+define("DB_NAME", "bfxr3414_zechifoumi");
 
 function connectDB()
 {
@@ -14,3 +14,22 @@ function connectDB()
 
     return $conn;
 }
+
+// local
+// <?php
+// define("DB_HOST", "localhost");
+// define("DB_USER", "root");
+// define("DB_PASSWORD", "");
+// define("DB_NAME", "zechifoumi");
+
+// function connectDB()
+// {
+//     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+//     if ($conn->connect_error) {
+//         die("La connexion à la base de données a échoué: " . $conn->connect_error);
+//     }
+
+//     return $conn;
+// }
+
