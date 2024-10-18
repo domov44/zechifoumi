49087f5c5462f14c0aac5cde3c648fe17f1bc09b
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-10-18 23:48:26 +0200

    [TEST] commit

--- authentification/config.php
+++ authentification/config.php
@@ -1,5 +1,4 @@
 <?php
-// Fonction pour charger le fichier .env et stocker les valeurs dans $_ENV
 function loadEnv($path) {
     if (!file_exists($path)) {
         throw new \Exception(".env file not found.");
@@ -8,19 +7,15 @@ function loadEnv($path) {
     $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     
     foreach ($lines as $line) {
-        // Ignorer les lignes de commentaire
         if (strpos(trim($line), '#') === 0) {
             continue;
         }
 
-        // Séparer la clé et la valeur
         list($name, $value) = explode('=', $line, 2);
 
-        // Enlever les guillemets et les espaces blancs autour de la valeur
         $name = trim($name);
         $value = trim($value);
 
-        // Définir la variable dans $_ENV
         $_ENV[$name] = $value;
     }
 }
