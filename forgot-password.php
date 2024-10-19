e6fb64dbcb53efbff5f29046e0ad31fc0786d203
Ronan Scotet <102806074+domov44@users.noreply.github.com>
2024-10-19 22:40:26 +0200

    [TEST]: php mailer

--- forgot-password.php
+++ forgot-password.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once('authentification/auth.php');
 require_once('authentification/session.php');
 require_once 'class/ToastHandler.php';
